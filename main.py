203268fc0c5fd91b52e0abb85248920d0ff88bb0
Juandiego001 <juandiego14012003@gmail.com>
2022-06-16 01:25:42 -0500

    Creando main y finalizando codigo

---
+++ main.py
@@ -0,0 +1,52 @@
+l = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'Ñ',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+]
+
+# m: mensaje a encriptar
+# d: desplazamiento
+# v: valor de acuerdo al diccionario
+# nM: nuevo mensaje
+def encrypt(m, d):
+    m = m.upper()
+    nM = ''
+    for i in m:
+        if i != ' ':
+            v = l.index(i)
+            v += d
+            nM += l[v]
+        else:
+            nM += ' '
+    
+    return nM
+
+me = input('Ingrese el mensaje: ')
+de = input('Ingrese el desplazamiento: ')
+
+print('\nMensaje con cifrado cesar: ' + encrypt(me, int(de)))
+        
