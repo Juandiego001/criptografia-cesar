661cdaa468caabc52fdc791b7d27e2fe487c94cb
Juandiego001 <juandiego14012003@gmail.com>
2022-06-16 10:14:34 -0500

    Agregando la función de desencriptar (primero se hizo la función de encriptar)

--- main.py
+++ main.py
@@ -28,6 +28,31 @@ l = [
     'Z'
 ]
 
+'''
+    Funcion para desencriptar mensaje
+'''
+def decrypt(m, d):
+    d *= -1
+    m = m.upper()
+    nM = ''
+    for i in m:
+        if i != ' ':
+            v = l.index(i)
+            v += d
+
+            if v > 26:
+                v -= 27
+
+            nM += l[v]
+        else:
+            nM += ' '
+    
+    return nM
+
+
+'''
+    Función para encriptar mensaje
+'''
 # m: mensaje a encriptar
 # d: desplazamiento
 # v: valor de acuerdo al arreglo
@@ -49,8 +74,14 @@ def encrypt(m, d):
     
     return nM
 
-me = input('Ingrese el mensaje: ')
+# Encriptado
+me = input('Ingrese el mensaje a encriptar: ')
 de = input('Ingrese el desplazamiento: ')
+print('Mensaje con cifrado cesar: ' + encrypt(me, int(de)))
 
-print('\nMensaje con cifrado cesar: ' + encrypt(me, int(de)))
+# Desencriptado (se toma el mismo desplazamiento)
+me = input('\nIngrese el mensaje a desencriptar: ')
+# Descomentar la línea de abajo para tomar un desplazamiento diferente
+# de = input('Ingrese el desplazamiento: ')
+print('Mensaje desencriptado: ' + decrypt(me, int(de)))
         
